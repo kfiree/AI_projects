ed4cc77b69769cdd622a49e7bce2dfa50b45a559
kfiree <kfirettinger@gmail.com>
2021-05-10 20:38:03 +0300

    set DFBnB variables

--- src/searchAlgorithm.java
+++ src/searchAlgorithm.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class searchAlgorithm {
 
@@ -276,6 +277,50 @@ public class searchAlgorithm {
 
     public void DFBnB(){
         System.out.println("DFBnB");
+/*
+
+DFBnB(Node start, Vector Goals)
+    1. L  make_stack(start) and H  make_hash_table(start)
+    2. result  null, t  ∞ // should be set to a strict upper bound in an infinite graph
+    3. While L is not empty
+        1. n  L.remove_front()
+        2. If n is marked as “out”
+            1. H.remove(n)
+        3. Else
+            1. mark n as “out” and L.insert(n)
+            2. N  apply all of the allowed operators on n
+            3. sort the nodes in N according to their f values (increasing order)
+            4. For each node g from N according to the order of N
+                1. If f(g) >= t
+                    1. remove g and all the nodes after it from N
+                2. Else If H contains g’=g and g’ is marked as “out”
+                    1. remove g from N
+                3. Else If H contains g’=g and g’ is not marked as “out”
+                    1. If f(g’)<=f(g)
+                        1. remove g from N
+                    2. Else
+                        1. remove g’ from L and from H
+                4. Else If goal(g) // if we reached here, f(g) < t
+                    1. t  f(g)
+                    2. result  path(g) // all the “out” nodes in L
+                    3. remove g and all the nodes after it from N
+            5. insert N in a reverse order to L and H
+        4. Return result
+*/
+        System.out.println("running DFBnB algorithm...");
+        stateNode start = this.board.getCurr();
+        stateNode goal = this.board.getGoal();
+        // 1. L  make_stack(start) and H  make_hash_table(start)
+        Stack<stateNode> L = new Stack<>();
+        Hashtable<String, stateNode> H = new Hashtable<>();
+
+        // 2. result  null, t  ∞ // should be set to a strict upper bound in an infinite graph
+        int t = Integer.MAX_VALUE;
+        List<stateNode> result = null;
+        // 3. While L is not empty
+        while(!L.isEmpty()){
+
+        }
     }
 
     private void HandlePath(stateNode pathEnd){
